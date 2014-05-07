56f23ff756aebd3493aaad58da240b550ba811ec
燕剑波 <yanshiyi1983@163.com>
2014-05-07 19:17:54 +0800

    trival change

--- src/nado_helper/core.clj
+++ src/nado_helper/core.clj
@@ -11,24 +11,23 @@
 
 (def ^:dynamic *db* (redis/init))
 
-(def ^:dynamic *uid* 1)
+(def ^:dynamic *uid* 10006)
 
 (defn init
-    [ & {:keys [url uid ] :or {url "redis://192.168.0.50:6380" uid *uid* }}]
+    [ & {:keys [url uid ] :or {url "redis://127.0.0.1:6379" uid *uid* }}]
     (def ^:dynamic *db* (redis/init {:url url}))
     (def ^:dynamic *uid* uid)
     (println url *uid*))
 
-(defn init-job [] (init :url "redis://192.168.0.50:6380" :uid 10006))
+(defn init-job [] (init :url "redis://192.168.0.41:6379" :uid 10006))
 
 (defn print-dict [d]
     (dorun
       (map #(println % (.get d %))
        (keys d))))
 
-
-(defn print-user
-    ([] (print-user *uid*))
+(defn show-user
+    ([] (show-user *uid*))
     ( [id] (print-dict (redis/hgetall *db*  (format "nadouser:%s:userinfo" id)))))
 
 (defn get-all [key]
@@ -40,7 +39,8 @@
     (print-dict (get-all key))
     (println "------------------------"))
 
-(declare print-slots)
+(declare print-slots show-list)
+
 (defn show-item
     ([pid] (show-item *uid* pid))
     ([uid pid]
@@ -48,7 +48,7 @@
         (print-slots 1)
         (print-slots 2)))
 
-(defn show-items []
+(defn show-items2 []
     (let [
             keys-format (format "nadouser:%s:item:*" *uid*)
             keys (redis/keys *db* keys-format)
@@ -58,22 +58,19 @@
 
 (def slots-keys {1 "nadouser:%s:propslots" 2 "nadouser:%s:runeslots"})
 
-(defn print-slots
- [item-type]
- (let [key (format (slots-keys item-type) *uid*)
-        length (redis/llen *db* key)
-    ]
+(defn print-slots [item-type]
+     (let [key (format (slots-keys item-type) *uid*)
+        length (redis/llen *db* key)]
     (println "------" key "-----------")
     (loop [i 0]
         (when (< i length)
             (println i (redis/lindex *db* key i))
             (recur (inc i))))))
 
-(defn show-items-and-slots []
-    (show-items)
-    (print-slots 1)
-    (print-slots 2))
-
+(defn show-slots [item-type]
+     (let [key (format (slots-keys item-type) *uid*)]
+        (println "------" key "-----------")
+        (show-list key)))
 
 (defn show-field [key field]
     (println  field (redis/hget *db* key field)))
@@ -88,7 +85,7 @@
   (let [user-key (format "nadouser:%s:userinfo" *uid*)
         num "100000"]
     (when (redis/exists *db* user-key)
-    (redis/hset *db* user-key "Money" num) 
+    (redis/hset *db* user-key "Money" num)
     (redis/hset *db* user-key "LingShi" num))))
 
 (defn reload-self []
@@ -98,12 +95,32 @@
 (defn get-values [key fields]
     (apply redis/hmget (concat [*db* key] fields)))
 
-(defn show-items-in-table []
-    (let [
-            keys-format (format "nadouser:%s:item:*" *uid*)
-            keys (redis/keys *db* keys-format)
-            headers (redis/hkeys *db* (first keys))
-            values (map #(get-values % headers) keys)
-        ]
-        (display/print-table headers values)))
+(defn show-by-pattern [pattern]
+    (let [keys (redis/keys *db* pattern)]
+        (if-let [key (first keys)]
+            (let [headers (redis/hkeys *db* key)
+                  values (map #(get-values % headers) keys)]
+                (display/print-table headers values)))))
+
+(defn show-items []
+    (let [pattern (format "nadouser:%s:item:*:iteminfo" *uid*)]
+        (show-by-pattern pattern)))
+
+(defn show-items-and-slots []
+    (show-items)
+    (print-slots 1)
+    (print-slots 2))
+
+(defn show-battle-array []
+    (let [key (format "nadouser:%s:battlearray" *uid* )]
+       (show-list key)))
+
+(defn show-list [key]
+    (let [headers ["No." "Value"]
+          length (redis/llen *db* key)
+          values (map vector (range length) (redis/lrange *db* key 0 length))]
+          (if (> length 0)
+            (display/print-table headers values)
+            (println "没有数据")
+            )))
 
